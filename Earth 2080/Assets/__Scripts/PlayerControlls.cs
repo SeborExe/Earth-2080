7e5ac058dfc7a10c04f7856b440a7895230d6fe3
SeborExe <mix98@o2.pl>
2022-04-07 21:36:16 +0200

    Player movement
    player movement script

--- Earth 2080/Assets/__Scripts/PlayerControlls.cs
+++ Earth 2080/Assets/__Scripts/PlayerControlls.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class PlayerControlls : MonoBehaviour
 {
-    private void Update()
-    {
-        float horizontalThrow = Input.GetAxis("Horizontal");
-        Debug.Log(horizontalThrow);
+    [SerializeField] float controllSpeed = 20f;
+    [SerializeField] float positionPitchFactor = 2f;
+    [SerializeField] float controlPitchFactor = -15f;
+    [SerializeField] float positionYawFactor = 2.5f;
+    [SerializeField] float controlRawFactor = -20f;
 
-        float verticalThrow = Input.GetAxis("Vertical");
-        Debug.Log(verticalThrow);
+    float xThrow, yThrow;
+
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
+
+        float xoffset = xThrow * Time.deltaTime * controllSpeed;
+        float newXpos = transform.localPosition.x + xoffset;
+        float newXposClamped = Mathf.Clamp(newXpos, -10.5f, 10.5f);
+
+        float yoffset = yThrow * Time.deltaTime * controllSpeed;
+        float newYpos = transform.localPosition.y + yoffset;
+        float newYposClamped = Mathf.Clamp(newYpos, -5, 8);
+
+        transform.localPosition = new Vector3(newXposClamped, newYposClamped, transform.localPosition.z);
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRawFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
