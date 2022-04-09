77f55c741fe1c36a025c5f8f5409eaf33da6bfbf
SeborExe <mix98@o2.pl>
2022-04-09 19:33:47 +0200

    ScoreBoard
    score system has been added.

--- Earth 2080/Assets/__Scripts/Enemy.cs
+++ Earth 2080/Assets/__Scripts/Enemy.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject exposionParticle;
+    [SerializeField] int score = 10;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         var particle = Instantiate(exposionParticle, transform.position, Quaternion.identity);
         Destroy(particle, 2f);
+
+        scoreBoard.IncreaseScore(score);
+
         Destroy(this.gameObject);
     }
 }
