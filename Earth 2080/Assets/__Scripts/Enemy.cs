1a3ef4e459e411981ea4f05cfd978d6a6fb0ab17
SeborExe <mix98@o2.pl>
2022-04-09 19:57:05 +0200

    Hit points
    Now enemy has hp and particles when he taking damage has been added.

--- Earth 2080/Assets/__Scripts/Enemy.cs
+++ Earth 2080/Assets/__Scripts/Enemy.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] GameObject exposionParticle;
+    [SerializeField] GameObject exposionParticle, hitParticles;
     [SerializeField] int score = 10;
+    [SerializeField] int hitPoints = 2;
+    int currentHitPoints;
 
     ScoreBoard scoreBoard;
 
     private void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        currentHitPoints = hitPoints;
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        var particle = Instantiate(exposionParticle, transform.position, Quaternion.identity);
-        Destroy(particle, 2f);
-
+        currentHitPoints--;
         scoreBoard.IncreaseScore(score);
 
+        var particle = Instantiate(hitParticles, transform.position, Quaternion.identity);
+        Destroy(particle, 1f);
+
+        if (currentHitPoints <= 0)
+            DestroyEnemy();
+    }
+
+    void DestroyEnemy()
+    {
+        var particle = Instantiate(exposionParticle, transform.position, Quaternion.identity);
+        Destroy(particle, 2f);
         Destroy(this.gameObject);
     }
 }
